8a1b6181be7e5d88baf0822bcbaa7244e6862b2d
Jacob Bieker <jacob@bieker.tech>
2021-10-12 15:20:58 +0100

    Copy over basics from Nowcasting Dataset

---
+++ setup.py
@@ -0,0 +1,23 @@
+""" Usual setup file for package """
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="nowcasting_dataloader",
+    version="0.0.1",
+    license="MIT",
+    description="Nowcasting Dataset",
+    author="Jack Kelly, Peter Dudfield, Jacob Bieker",
+    author_email="info@openclimatefix.org",
+    company="Open Climate Fix Ltd",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
