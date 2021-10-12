6f39a2de27c0be0e8559f1094f2d1efeabcda5fb
Jacob Bieker <jacob@bieker.tech>
2021-10-12 16:26:40 +0100

    Add missing subselect_data

--- nowcasting_dataloader/dataloader.py
+++ nowcasting_dataloader/dataloader.py
@@ -227,3 +227,94 @@ class NetCDFDataset(torch.utils.data.Dataset):
         batch.change_type_to_numpy()
 
         return batch
+
+def subselect_data(
+        batch: Batch,
+        required_keys: Union[Tuple[str], List[str]],
+        history_minutes: int,
+        forecast_minutes: int,
+        current_timestep_index: Optional[int] = None,
+) -> Batch:
+    """
+    Subselects the data temporally. This function selects all data within the time range [t0 - history_minutes, t0 + forecast_minutes]
+
+    Args:
+        batch: Example dictionary containing at least the required_keys
+        required_keys: The required keys present in the dictionary to use
+        current_timestep_index: The index into either SATELLITE_DATETIME_INDEX or NWP_TARGET_TIME giving the current timestep
+        history_minutes: How many minutes of history to use
+        forecast_minutes: How many minutes of future data to use for forecasting
+
+    Returns:
+        Example with only data between [t0 - history_minutes, t0 + forecast_minutes] remaining
+    """
+    _LOG.debug(
+        f"Select sub data with new historic minutes of {history_minutes} "
+        f"and forecast minutes if {forecast_minutes}"
+    )
+
+    # We are subsetting the data, so we need to select the t0_dt, i.e the time now for eahc Example.
+    # We infact only need this from the first example in each batch
+    if current_timestep_index is None:
+        # t0_dt or if not available use a different datetime index
+        t0_dt_of_first_example = batch.metadata.t0_dt[0].values
+    else:
+        if SATELLITE_DATA in required_keys:
+            t0_dt_of_first_example = batch.satellite.sat_datetime_index[
+                0, current_timestep_index
+            ].values
+        else:
+            t0_dt_of_first_example = batch.satellite.sat_datetime_index[
+                0, current_timestep_index
+            ].values
+
+    # make this a datetime object
+    t0_dt_of_first_example = pd.to_datetime(t0_dt_of_first_example)
+
+    if batch.satellite is not None:
+        batch.satellite.select_time_period(
+            keys=[SATELLITE_DATA, SATELLITE_DATETIME_INDEX],
+            history_minutes=history_minutes,
+            forecast_minutes=forecast_minutes,
+            t0_dt_of_first_example=t0_dt_of_first_example,
+        )
+
+    # Now for NWP, if used
+    if batch.nwp is not None:
+        batch.nwp.select_time_period(
+            keys=[NWP_DATA, NWP_TARGET_TIME],
+            history_minutes=history_minutes,
+            forecast_minutes=forecast_minutes,
+            t0_dt_of_first_example=t0_dt_of_first_example,
+        )
+    #
+    # Now for GSP, if used
+    if batch.gsp is not None:
+        batch.gsp.select_time_period(
+            keys=[GSP_DATETIME_INDEX, GSP_YIELD],
+            history_minutes=history_minutes,
+            forecast_minutes=forecast_minutes,
+            t0_dt_of_first_example=t0_dt_of_first_example,
+        )
+
+    # Now for PV, if used
+    if batch.pv is not None:
+        batch.pv.select_time_period(
+            keys=[PV_DATETIME_INDEX, PV_YIELD],
+            history_minutes=history_minutes,
+            forecast_minutes=forecast_minutes,
+            t0_dt_of_first_example=t0_dt_of_first_example,
+        )
+
+    # Now for SUN, if used
+    if batch.sun is not None:
+        batch.sun.select_time_period(
+            keys=[SUN_ELEVATION_ANGLE, SUN_AZIMUTH_ANGLE],
+            history_minutes=history_minutes,
+            forecast_minutes=forecast_minutes,
+            t0_dt_of_first_example=t0_dt_of_first_example,
+        )
+
+    # DATETIME TODO
+
+    return batch
