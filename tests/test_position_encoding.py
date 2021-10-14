61a53471c8a0f467f151d694426e2716d040d498
Jacob Bieker <jacob@bieker.tech>
2021-10-14 08:22:18 +0100

    Remove stub tests

--- tests/test_position_encoding.py
+++ tests/test_position_encoding.py
@@ -174,11 +174,3 @@ def test_fake_method_option():
 def test_fake_positioning_option():
     with pytest.raises(AssertionError):
         encode_position(shape=[1, 1, 1, 1], positioning="fake positioning scheme")
-
-
-def test_multi_modality_encoding():
-    pass
-
-
-def test_5min_30min_encoding():
-    pass
