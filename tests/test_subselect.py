ecf786b23cb71bd7b26663344393bbe3c3fc9893
Jacob Bieker <jacob@bieker.tech>
2021-10-28 16:58:29 +0100

    Fix test issues

--- tests/test_subselect.py
+++ tests/test_subselect.py
@@ -1,10 +1,23 @@
+"""Test subselecting functions """
+import pytest
+from nowcasting_dataset.config.model import Configuration, InputData
 from nowcasting_dataset.dataset.batch import Batch
+
 from nowcasting_dataloader.subset import subselect_data
 
 
-def test_subselect_date(test_data_folder):
+@pytest.fixture
+def configuration():
+    """Create Configuration for test"""
+    con = Configuration()
+    con.input_data = InputData.set_all_to_defaults()
+    con.process.batch_size = 4
+    return con
 
-    x = Batch.fake()
+
+def test_subselect_date(test_data_folder, configuration):
+    """Test subselecting Data"""
+    x = Batch.fake(configuration=configuration)
 
     batch = subselect_data(
         x,
@@ -18,9 +31,9 @@ def test_subselect_date(test_data_folder):
 
 
 #
-def test_subselect_date_with_to_dt(test_data_folder):
-
-    x = Batch.fake()
+def test_subselect_date_with_to_dt(test_data_folder, configuration):
+    """Test subselecting Data with datetimes"""
+    x = Batch.fake(configuration=configuration)
 
     batch = subselect_data(
         x,
