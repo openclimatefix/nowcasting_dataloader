0715928b8611c1faee0459c60d1a5c97b0f3905e
Jacob Bieker <jacob@bieker.tech>
2021-10-29 13:17:10 +0100

    Update test data

--- scripts/generate_data_for_tests/get_test_data.py
+++ scripts/generate_data_for_tests/get_test_data.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 
-from nowcasting_dataset.config.model import Configuration
+from nowcasting_dataset.config.model import Configuration, InputData
 from nowcasting_dataset.dataset.batch import Batch
 
 import nowcasting_dataloader
@@ -15,9 +15,10 @@ local_path = os.path.dirname(nowcasting_dataloader.__file__) + "/.."
 ########
 
 c = Configuration()
+c.input_data = InputData.set_all_to_defaults()
 c.process.batch_size = 4
 c.input_data.nwp.nwp_channels = c.input_data.nwp.nwp_channels[0:1]
-c.input_data.satellite.sat_channels = c.input_data.satellite.sat_channels[0:1]
+c.input_data.satellite.satellite_channels = c.input_data.satellite.satellite_channels[0:1]
 
 f = Batch.fake(configuration=c)
 f.save_netcdf(batch_i=0, path=Path(f"{local_path}/tests/data/batch"))
