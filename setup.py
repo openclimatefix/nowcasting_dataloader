a117cff5056be7c886a155973d7ac41062051398
BumpVersion Action <bumpversion@github-actions>
2021-11-17 17:15:11 +0000

    Bump version: 1.3.0 → 1.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="nowcasting_dataloader",
-    version="1.3.0",
+    version="1.3.1",
     license="MIT",
     description="Nowcasting Dataloader",
     author="Jack Kelly, Peter Dudfield, Jacob Bieker",
