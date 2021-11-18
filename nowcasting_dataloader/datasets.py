388b1127387ab7db03fa9fac688c4c6de8a8527c
Jacob Bieker <jacob@bieker.tech>
2021-11-18 14:13:21 +0000

    Add in the GSP yield query to the inputs

--- nowcasting_dataloader/datasets.py
+++ nowcasting_dataloader/datasets.py
@@ -326,6 +326,11 @@ class SatFlowDataset(NetCDFDataset):
                 x = self.add_encodings(x, TOPOGRAPHIC_DATA, batch, 0, False)
             if x.get(NWP_DATA, False):
                 x = self.add_encodings(x, NWP_DATA, batch, len(x[NWP_DATA]), False)
+            if x.get(PV_YIELD, False):
+                x = self.add_encodings(x, PV_YIELD, batch, self.current_timestep_index, False)
+            # Add the future GSP position encoding for querying
+            x[GSP_YIELD+"_query"] = batch[GSP_YIELD+"_position_encoding"][:, :, :self.current_timestep_index_30]
+
 
         return x, target
 
