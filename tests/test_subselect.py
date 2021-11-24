20edd3333f5f25e9048ffa61c28028868343188f
Jack Kelly <jack@openclimatefix.org>
2021-11-24 16:52:58 +0000

    fix tests now that we are only using 10 NWP chans

--- tests/test_subselect.py
+++ tests/test_subselect.py
@@ -27,7 +27,7 @@ def test_subselect_date(test_data_folder, configuration):
     )
 
     assert batch.satellite.data.shape == (4, 19, 64, 64, 10)
-    assert batch.nwp.data.shape == (4, 2, 64, 64, 17)
+    assert batch.nwp.data.shape == (4, 2, 64, 64, 10)
     assert batch.pv.x_coords.shape == (4, 128)
 
 
